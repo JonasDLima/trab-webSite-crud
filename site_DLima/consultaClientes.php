c1674958fafe05cfe69e47be488acdea677e76f1
JonasDLima <prg.jonasdlima@gmail.com>
2022-01-16 08:29:33 -0300

    Finalizacao do layout basico das telas de consulta

--- site_DLima/consultaClientes.php
+++ site_DLima/consultaClientes.php
@@ -1,7 +1,57 @@
 <?php
     include_once 'includes/header.php';
 ?>
+        <div class="form-default">
+          <div class="col s12 m6">
+              <div class="card amber lighten-3 content-border-default">
+                <div class="card-content white-text form-login-data">
+                    <span class="card-title" style="color: #000 !important;">Listagem de Clientes</span>
+                    <table class="responsive-table">
+                    <thead>
+                      <tr>
+                          <th>Name</th>
+                          <th>Telefone</th>
+                          <th>Endereço</th>
+                      </tr>
+                    </thead>
 
+                    <tbody>
+                      <tr>
+                        <td>Alvin de Souza Neves</td>
+                        <td>21989898989</td>
+                        <td>Seila, Rua dos Bobos, 0</td>
+                      </tr>
+                      <tr>
+                        <td>Alan da Silva</td>
+                        <td>27995253608</td>
+                        <td>Jardins, Rua do Manguesal, 309</td>
+                      </tr>
+                      <tr>
+                        <td>Jonathan Jr. da Silva Chavier</td>
+                        <td>3199745265</td>
+                        <td>São Pedro, Buraca, 80</td>
+                      </tr>
+                      <tr>
+                        <td>Jonathan Medeiros Brabo</td>
+                        <td>27985993609</td>
+                        <td>Morro do 4, Travesa da dificuldade, 1</td>
+                      </tr>
+                      <tr>
+                        <td>Juquinha Fosh</td>
+                        <td>25999654526</td>
+                        <td>Boa pergunta, Zona de beozionte, 56</td>
+                      </tr>
+                      <tr>
+                        <td>Juninho Play</td>
+                        <td>29756898368</td>
+                        <td>Nice Guy, Vida Loka, 10</td>
+                      </tr>
+                    </tbody>
+                    </table>
+                </div>
+              </div>
+            </div>
+        <div>
 <?php
     include_once 'includes/footer.php';
 ?>      
---
+++ site_DLima/consultaProdutos.php
@@ -0,0 +1,57 @@
+<?php
+    include_once 'includes/header.php';
+?>
+        <div class="form-default">
+          <div class="col s12 m6">
+              <div class="card amber lighten-3 content-border-default">
+                <div class="card-content white-text form-login-data">
+                    <span class="card-title" style="color: #000 !important;">Listagem de Produtos</span>
+                    <table class="responsive-table">
+                    <thead>
+                      <tr>
+                          <th>Name</th>
+                          <th>Item Name</th>
+                          <th>Item Price</th>
+                      </tr>
+                    </thead>
+
+                    <tbody>
+                      <tr>
+                        <td>Alvin</td>
+                        <td>Eclair</td>
+                        <td>$0.87</td>
+                      </tr>
+                      <tr>
+                        <td>Alan</td>
+                        <td>Jellybean</td>
+                        <td>$3.76</td>
+                      </tr>
+                      <tr>
+                        <td>Jonathan</td>
+                        <td>Lollipop</td>
+                        <td>$7.00</td>
+                      </tr>
+                      <tr>
+                        <td>Jonathan</td>
+                        <td>Lollipop</td>
+                        <td>$7.00</td>
+                      </tr>
+                      <tr>
+                        <td>Jonathan</td>
+                        <td>Lollipop</td>
+                        <td>$7.00</td>
+                      </tr>
+                      <tr>
+                        <td>Jonathan</td>
+                        <td>Lollipop</td>
+                        <td>$7.00</td>
+                      </tr>
+                    </tbody>
+                    </table>
+                </div>
+              </div>
+            </div>
+        <div>
+<?php
+    include_once 'includes/footer.php';
+?>      
